d8ba51f1c40a74d801eff33c4728f8aeb1ac7b58
speed1313 <speedtry13@icloud.com>
2021-12-19 19:16:45 +0900

    impl simple CLI

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,74 @@
+extern crate clap;
+mod repl;
+
+use repl::{REPLHelper, get_config};
+
+
 use rustyline::error::ReadlineError;
-use rustyline::Editor;
+use rustyline::{Editor};
 
-fn main() {
-    
-    let mut repl = Editor::<()>::new();
+use clap::{App, crate_version};
 
-    if repl.load_history("history.txt").is_err(){
+fn main()->rustyline::Result<()> {
+    env_logger::init();
+
+    let _matches = App::new("mini-sqlite")
+                          .version("0.0.1")
+                          .author("speed1313")
+                          .about("mini-sqlite for study")
+                          .get_matches();
+
+    let config = get_config();
+
+    let helper = REPLHelper::new();
+
+    let mut repl = Editor::with_config(config);
+    repl.set_helper(Some(helper));
+
+    if repl.load_history("history").is_err(){
         println!("No previous history.");
     }
-    loop{
-        let readline = repl.readline(">> ");
 
+    let mut count = 1;
+    loop{
+        if count == 1 {
+            println!("{}{}{}{}{}",
+                format!("mini-sqlite-{}\n",crate_version!()),
+                "Enter .exit to quit.\n",
+                "Enter .help for usage hints.\n",
+                "Connected to a transient in-memory database.\n",
+                "Use '.open FILENAME' to reopen on a persistent database.");
+        }
+
+        let p = format!("mini-sqlite | {}> ", count);
+        repl.helper_mut()
+            .expect("No helper found")
+            .colored_prompt = format!("\x1b[1;32m{}\x1b[0m", p );
+
+        let readline = repl.readline(&p);
         match readline {
-            Ok(line)=>{
-                repl.add_history_entry(line.as_str());
-                println!("Line: {}",line);
+            Ok(command)=>{
+                repl.add_history_entry(command.as_str());
+                if command.eq(".exit"){
+                    break;
+                }else{
+                    println!("Error: unknown command or invalid arguments: '{}'. Enter '.help'",&command);
+                }
             },
             Err(ReadlineError::Interrupted) => {
-                println!("CTRL-C");
                 break
             },
             Err(ReadlineError::Eof)=>{
-                println!("CTRL-D");
                 break;
             },
             Err(err)=>{
                 println!("Error: {:?}",err);
-                break
+                break;
             }
         }
+        count += 1;
     }
+    repl.save_history("history").unwrap();
 
-    repl.save_history("history.txt").unwrap();
+    Ok(())
 }
