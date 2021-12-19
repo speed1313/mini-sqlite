391156b25fb2166ac970f18e279b643f403a6429
speed1313 <speedtry13@icloud.com>
2021-12-19 18:42:40 +0900

    impl repl feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let mut repl = Editor::<()>::new();
+
+    if repl.load_history("history.txt").is_err(){
+        println!("No previous history.");
+    }
+    loop{
+        let readline = repl.readline(">> ");
+
+        match readline {
+            Ok(line)=>{
+                repl.add_history_entry(line.as_str());
+                println!("Line: {}",line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof)=>{
+                println!("CTRL-D");
+                break;
+            },
+            Err(err)=>{
+                println!("Error: {:?}",err);
+                break
+            }
+        }
+    }
+
+    repl.save_history("history.txt").unwrap();
 }
